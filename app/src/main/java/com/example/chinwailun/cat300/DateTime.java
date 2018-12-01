86ddb0d4b48e0c09ee1bd53e393b25609e92cf6e
Zhen Li <zhenli@student.usm.my>
2018-12-01 13:28:53 +0800

    crowd analysis update

--- app/src/main/java/com/example/chinwailun/cat300/DateTime.java
+++ app/src/main/java/com/example/chinwailun/cat300/DateTime.java
@@ -14,38 +14,49 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class DateTime extends AppCompatActivity {
-
+    public static final String EXTRA_DATE = "com.example.chinwailun.cat300.EXTRA_TEXT";
     private DatePickerDialog.OnDateSetListener mDateSetListener;
     private TimePickerDialog.OnTimeSetListener mTimeSetListener;
     private TextView mSetDate;
+    private TextView mSetTime;
     private TextView mDisplayDate;
+    private TextView mDisplayTime;
     private TextView mInterval;
     private TextView mHour;
     private Spinner mSpinner;
     private Button mButton;
-
-
-    public void sometesting(){}
+    int mdayofmonth, mmonth, myear, mhour, mminute;
+    String format;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_time);
         mDisplayDate = (TextView) findViewById(R.id.displayDate);
+        mDisplayTime = (TextView) findViewById(R.id.displayTime);
         mSetDate = (TextView) findViewById(R.id.setDate);
-        mInterval = (TextView) findViewById(R.id.interval);
-        mSpinner = (Spinner) findViewById(R.id.spinner);
+        mSetTime = (TextView) findViewById(R.id.setTime);
         mButton = (Button) findViewById(R.id.goButton);
 
-        String[] intervalNum = {"1","2","3","4","5"};
-        ArrayAdapter<CharSequence> langAdapter = new ArrayAdapter<CharSequence>(DateTime.this, R.layout.support_simple_spinner_dropdown_item, intervalNum);
-        langAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        mSpinner.setAdapter(langAdapter);
+
+        mSetTime.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int hour = 0, minute = 0;
+                boolean is24 = true;
+
+                TimePickerDialog tDialog = new TimePickerDialog(DateTime.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth, mTimeSetListener, hour, minute,is24);
+                tDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                tDialog.show();
+            }
+        });
 
         mSetDate.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -62,41 +73,52 @@ public class DateTime extends AppCompatActivity {
             }
         });
 
-        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                mSpinner.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mSpinner.setSelection(position);
-                    }
-                });
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
         mDateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
                 month += 1;
-                String date = dayOfMonth + "/" +  month + "/" + year;
+                String date = dayOfMonth + "-" +  month + "-" + year;
+                mdayofmonth = dayOfMonth;
+                mmonth = month -1;
+                myear = year;
                 mDisplayDate.setText(date);
+            }
+        };
+
+        mTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String time;
+
+                if(minute <10)
+                    time = hourOfDay + ":0" + minute;
+                else
+                    time = hourOfDay + ":" + minute;
+                mhour = hourOfDay;
+                mminute = minute;
+                mDisplayTime.setText(time);
             }
         };
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ArrayList<String> data = new ArrayList<>();
-                data.add(mSpinner.getSelectedItem().toString());
-                data.add((String) mDisplayDate.getText());
-                //Toast.makeText(DateTime.this, (String) mDisplayDate.getText() + " ------- " + mSpinner.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+                ArrayList<Integer> data = new ArrayList<>();
+
+                /*data.add(myear);
+                data.add(mmonth);
+                data.add(mdayofmonth);
+                data.add(mhour);
+                data.add(mminute);*/
+
+                data.add(2018);
+                data.add(10);
+                data.add(30);
+                data.add(12);
+                data.add(0);
+
                 Intent mI = new Intent(DateTime.this,MapsActivity.class);
+                mI.putExtra(EXTRA_DATE, data);
                 startActivity(mI);
             }
         });
--- app/src/main/java/com/example/chinwailun/cat300/MapsActivity.java
+++ app/src/main/java/com/example/chinwailun/cat300/MapsActivity.java
@@ -62,6 +62,8 @@ import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     String receiveDate;
     Date date;
+    int numOfDoc = 0;
+    final Handler handler = new Handler();
     private GoogleMap mMap;
     private LocationRequest locReq;
     private LocationCallback locCallback;
@@ -79,6 +81,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        mapFragment.getView().setClickable(true);
 
         Intent i = getIntent();
         //receiveDate = i.getStringExtra(DateTime.EXTRA_DATE);
@@ -86,6 +89,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         boolean firstRun = true;
 
         client = LocationServices.getFusedLocationProviderClient(this);
+
+
 
         locReq = new LocationRequest();
         locReq.setInterval(5000);
@@ -123,166 +128,94 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         processData();
-        //testfunc();
     }
-
-    public void testfunc()
-    {
-        Date calDate;
-        Calendar cal = Calendar.getInstance();
-        cal.set(2018, 10, 30, 12, 0);
-        //cal.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
-        ;
-        calDate = cal.getTime();
-
-        DocumentReference dr = db.collection("Users").document("Alex Chong").collection("Schedule").document("Pizza Hut");
-        dr.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                Log.d("log1", ""+ task.getResult().getTimestamp("Start").toDate().getTime());
-                Log.d("log2", ""+ calDate.getTime());
-            }
-        });
-    }
-
 
     public void processData()
     {
-        ArrayList<DocumentSnapshot> docList = new ArrayList<>();
-        ArrayList<GeoPoint> geoList = new ArrayList<GeoPoint>();
+        int numDoc = 0;
         Calendar calTerminate = Calendar.getInstance();
         Calendar calGiven = Calendar.getInstance();
         calTerminate.set(rDate.get(0), rDate.get(1), rDate.get(2), 0, 0, 0);
         calTerminate.add(Calendar.DAY_OF_MONTH, 1);
         calGiven.set(rDate.get(0), rDate.get(1), rDate.get(2), rDate.get(3), rDate.get(4), 0);
         calGiven.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+        ArrayList<DocumentSnapshot> docList = new ArrayList<>();
 
-        Handler handler = new Handler();
-        while(calGiven.before(calTerminate))
-        {
-            calGiven.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
-            Date givenDate = calGiven.getTime(); // get dynamic calendar
-            Timestamp timestampDynamic = new Timestamp(givenDate);
-            timestampDynamic.toDate().getTime();
+        db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    int numOfUser = task.getResult().size();
+                    for (QueryDocumentSnapshot col : task.getResult()) {
+                        numOfUser--;
+                        final int userNum = numOfUser;
+                        final String userDoc = col.getId();
+                        db.collection("Users").document(userDoc).collection("Schedule").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    int numOfDoc = task.getResult().size();
+                                    for (DocumentSnapshot place : task.getResult()) {
+                                        docList.add(place);
+                                        numOfDoc--;
 
-            db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if(task.isSuccessful())
-                    {
-                        for(QueryDocumentSnapshot col : task.getResult())
-                        {
-                            final String userDoc = col.getId();
-                            db.collection("Users").document(userDoc).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task task) {
-                                    if (task.isSuccessful()) {
-                                        db.collection("Users").document(userDoc).collection("Schedule").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                if(task.isSuccessful())
-                                                {
-                                                    for(DocumentSnapshot place : task.getResult())
-                                                    {
-                                                        if(place.getTimestamp("Start").toDate().before(givenDate) &&
-                                                                place.getTimestamp("End").toDate().after(givenDate))
-                                                        {
-                                                            //docList.add(place);
-                                                            GeoPoint coordinate = place.getGeoPoint("Coordinate");
-                                                            LatLng location = new LatLng(coordinate.getLatitude(), coordinate.getLongitude());
-                                                            mMap.addMarker(new MarkerOptions().position(location));
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        });
+                                        if (numOfDoc == 0 && userNum == 0) {
+                                            filterDoc(docList);
+                                        }
                                     }
                                 }
-                            });
-                        }
+                            }
+                        });
                     }
                 }
-            });
+            }
+        });
+    }
 
-            //mMap.clear();
-            /*for(int i = 0; i < docList.size(); i++)
-            {
-                if(docList.get(i).getTimestamp("Start").toDate().before(givenDate)
-                        && docList.get(i).getTimestamp("End").toDate().after(givenDate))
-                {
-                    GeoPoint coordinate = docList.get(i).getGeoPoint("Coordinate");
-                    LatLng location = new LatLng(coordinate.getLatitude(), coordinate.getLongitude());
-                    mMap.addMarker(new MarkerOptions().position(location));
-                    docList.remove(i);
-                    i--;
+
+    public void filterDoc(ArrayList<DocumentSnapshot> docList)
+    {
+        Calendar calTerminate = Calendar.getInstance();
+        Calendar calGiven = Calendar.getInstance();
+        calTerminate.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+        calGiven.setTimeZone(TimeZone.getTimeZone("GMT+08:00"));
+        calTerminate.set(rDate.get(0), rDate.get(1), rDate.get(2), 0, 0, 0);
+        calTerminate.add(Calendar.DAY_OF_MONTH, 1);
+        calGiven.set(rDate.get(0), rDate.get(1), rDate.get(2), rDate.get(3), rDate.get(4), 0);
+
+        while(calGiven.before(calTerminate)) {
+
+            Date givenDate = calGiven.getTime(); // get dynamic calendar
+
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    plotMap(docList, givenDate);
                 }
-            }*/
+            },5000);
 
+            //Toast.makeText(this, givenDate.toString(), Toast.LENGTH_LONG).show();
             calGiven.add(Calendar.HOUR_OF_DAY, 1); // add one hour to dynamic calendar
         }
-            //Toast.makeText(this, givenDate.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public void plotMap(ArrayList<DocumentSnapshot> docList, Date givenDate)
+    {
+        Toast.makeText(MapsActivity.this, givenDate.toString(), Toast.LENGTH_LONG).show();
+        if(docList.size() != 0)
+            mMap.clear();
+
+        for(int i = 0; i < docList.size(); i++)
+        {
+            if(docList.get(i).getTimestamp("Start").toDate().before(givenDate)
+                    && docList.get(i).getTimestamp("End").toDate().after(givenDate))
+            {
+                GeoPoint coordinate = docList.get(i).getGeoPoint("Coordinate");
+                LatLng location = new LatLng(coordinate.getLatitude(), coordinate.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(location));
+                docList.remove(i);
+                i--;
+            }
+        }
     }
 }
-        //Toast.makeText(this, "Done", Toast.LENGTH_LONG).show();
-//}
-
-/*while(c2.before(cFix))
-        {   // have to add handler somewhere here to handle delayed order
-            mMap.clear();
-            db.collection("Users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if(task.isSuccessful())
-                    {
-                        for(QueryDocumentSnapshot col : task.getResult())
-                        {
-                            final String userDoc = col.getId();
-                            db.collection("Users").document(userDoc).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                @Override
-                                public void onComplete(@NonNull Task task) {
-                                    if(task.isSuccessful())
-                                    {
-                                        db.collection("Users").document(userDoc).collection("Schedule").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                if(task.isSuccessful())
-                                                {
-                                                    for(QueryDocumentSnapshot place : task.getResult())
-                                                    {
-                                                        final String placeDoc = place.getId();
-                                                        db.collection("Users").document(userDoc).collection("Schedule").document(placeDoc).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>(){
-                                                            @Override
-                                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                                if(task.isSuccessful())
-                                                                {
-                                                                    DocumentReference ref = db.collection("Users").document(userDoc).collection("Schedule").document(placeDoc);
-                                                                    ref.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                                                        @Override
-                                                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                                            time = ((Timestamp)task.getResult().getData().get("Date & Time"));
-                                                                            if((time.toDate()).equals(c2) && (time.toDate()).before(cFix.getTime())
-                                                                                    || (time.toDate()).after(c2.getTime()) && (time.toDate()).before(cFix.getTime()))
-                                                                            {
-                                                                                GeoPoint coordinate = null;
-                                                                                coordinate = (GeoPoint) task.getResult().getData().get("Coordinate");
-                                                                                LatLng sydney = new LatLng(coordinate.getLatitude(), coordinate.getLongitude());
-                                                                                mMap.addMarker(new MarkerOptions().position(sydney));
-                                                                            }
-                                                                        }
-                                                                    });
-                                                                }
-                                                            }
-                                                        });
-                                                    }
-                                                }
-                                            }
-                                        });
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-            });
-            c2.add(Calendar.HOUR, 1);
-        }*/
